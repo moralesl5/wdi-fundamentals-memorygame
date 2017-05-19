ea99c35f45f41f2a0de6016c7506c1a87ed95e07
moralesl5 <loganjmorales@gmail.com>
2017-05-19 00:13:25 -0400

    Copied files to correct repo. Updates include: Created game logic, css and html files from prior work in project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+ var cardOne = "queen";
+ var cardTwo = "king";
+ var cardThree = "king";
+ var cardFour = "queen"; 
+
+ console.log("Up and running!");
+ console.log("User flipped " + cardThree);
+ console.log("User flipped " + cardFour);
