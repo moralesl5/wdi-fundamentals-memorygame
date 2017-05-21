b6f77b6c0726490d6f8af531ac7c7b675d1e516e
moralesl5 <loganjmorales@gmail.com>
2017-05-20 23:16:31 -0400

    created game logic by adding in organization to main.js through the use of functions

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+var checkForMatch = function(){
+	if (cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]){
+			alert("You found a match!");
+		}
+		else{
+			alert("Sorry, try again.");
+		}
+	};
+};
 
-if (cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	}
-	else{
-		alert("Sorry, try again.");
-	}
-}
+
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+
+
 
